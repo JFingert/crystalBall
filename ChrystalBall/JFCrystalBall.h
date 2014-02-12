dd280010b39cb72022f6d599998f6d0f6d894396
JFingert <joshuafingert@gmail.com>
2014-02-12 14:24:35 -0800

    first commit

---
+++ ChrystalBall/JFCrystalBall.h
@@ -0,0 +1,13 @@
+//
+//  JFCrystalBall.h
+//  ChrystalBall
+//
+//  Created by Joshua Fingert on 2/6/14.
+//  Copyright (c) 2014 JoshuaFingert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFCrystalBall : NSObject
+
+@end
---
+++ ChrystalBall/JFCrystalBall.m
@@ -0,0 +1,13 @@
+//
+//  JFCrystalBall.m
+//  ChrystalBall
+//
+//  Created by Joshua Fingert on 2/6/14.
+//  Copyright (c) 2014 JoshuaFingert. All rights reserved.
+//
+
+#import "JFCrystalBall.h"
+
+@implementation JFCrystalBall
+
+@end
